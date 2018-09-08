3c8135c4555d8a532f6f4c795cb180728bc6e97c
Zul Hilmi <netspytux@gmail.com>
2018-09-08 14:50:51 +0700

    ...

--- layout/stats.php
+++ layout/stats.php
@@ -1,17 +1,1 @@
-<table class="table table-bordered">
-  <thead>
-    <tr>
-      <th>KECAMATA</th>
-      <th>DATA</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-  $q = mysqli_query($conn, "select * from tb_pemilik_kendaraan where kecamatan='CIAMPEL'");
-  while($r = mysqli_fetch_array($q)) { ?>
-    <tr>
-      <th></th>
-    </tr>
-  <?php } ?>
-  </tbody>
-</table>
+kosong. lieur.
