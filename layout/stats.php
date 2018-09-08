8ab047ac89d7201a9aa2e2be9cdab4b3d35a8337
Zul Hilmi <netspytux@gmail.com>
2018-09-08 16:14:00 +0700

    manual heula sementawis mah

--- layout/stats.php
+++ layout/stats.php
@@ -1,1 +1,9 @@
-kosong. lieur.
+<div class="row">
+  <div class="col-md-12">
+    <div class="card card-plain">
+      <div class="content">
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1LsoGVWqk5bpP7scbd8MWSEYnU41NRIJk" width="100%" height="500"></iframe>
+      </div>
+    </div>
+  </div>
+</div>
