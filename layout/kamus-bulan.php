adac16c41ebe2df2514bef0693d29140eea10b49
HilmiZul <netspytux@gmail.com>
2018-11-08 22:31:35 +0700

    first commit: kamus bulan :D

---
+++ layout/kamus-bulan.php
@@ -0,0 +1,16 @@
+<?php 
+// kamus bulan :D
+$month = [
+  'Januari',
+  'Februari',
+  'Maret',
+  'April',
+  'Mei',
+  'Juni',
+  'Juli',
+  'Agustus',
+  'September',
+  'Oktober',
+  'November',
+  'Desember',
+];
